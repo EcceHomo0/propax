5d19e68d07048575b79b19f0aac24fd42c3791b3
Yann <yanntieba@gmail.com>
2026-03-28 21:39:56 +0100

    Reparation du CI frontend et auto-initialisation de la base de données

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the home search button", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(await screen.findByRole("button", { name: /rechercher/i })).toBeInTheDocument();
 });
